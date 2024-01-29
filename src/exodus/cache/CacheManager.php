3df3826fe3ee1d537514d10dd6949c24b69a122a
srclauyt@gmail.com <srclauyt@gmail.com>
2024-01-29 16:35:15 -0300

    ready

---
+++ src/exodus/cache/CacheManager.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace exodus\cache;
+
+class CacheManager
+{
+  private array $caches = [];
+  
+  public function set(string $name, mixed $data): bool
+  {
+    if (empty($name) or isset($this->caches[$name])) {
+      return false;
+    }
+    $this->caches[$name] = new Cache($data);
+  }
+  
+  public function get(string $name): ?Cache
+  {
+    return $this->caches[$name] ?? null;
+  }
+  
+  public function getAll(): array
+  {
+    return $this->caches;
+  }
+  
+}
